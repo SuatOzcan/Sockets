6f3c6dfc8fb79fdefd6e502180e5790ea2347528
suatozcan <email@example.com>
2020-09-10 13:26:03 +0300

    SocketTest now recieves response from server, then disconnects and gets disposed.

--- SocketTest/Program.cs
+++ SocketTest/Program.cs
@@ -30,17 +30,27 @@ namespace SocketTest
                     var messageBytes = Encoding.ASCII.GetBytes(message);
                     var segment = new ArraySegment<byte>(messageBytes);
                     await socket.SendAsync(segment, SocketFlags.None);
-                }
+                } 
+            }
+
+            var receiveSeg = new ArraySegment<byte>(new byte[512], 0, 512);
+            await socket.ReceiveAsync(receiveSeg, SocketFlags.None);
+            string receivedMessage = Encoding.ASCII.GetString(receiveSeg);
+            foreach (var line in receivedMessage.Split())
+            {
+                Console.WriteLine(line);
             }
             
+            socket.Disconnect(false);
+            socket.Dispose();
         }
 
         private static string GetRequestMessage(string server, int port, string path)
         {
-            var message = $"GET {path} HTTP/1.1\r\n";
-            message += $"Host: {server}:{port}\r\n";
-            message += "cache-control: no-cache\r\n";
-            message += "\r\n";
+            var message = $"GET {path} HTTP/1.1";
+            message += $"Host: {server}:{port}";
+            message += "cache-control: no-cache";
+            message += "";
             return message;
         }
     }
